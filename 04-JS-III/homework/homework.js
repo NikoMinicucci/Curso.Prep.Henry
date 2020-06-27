85a829205551c0156ff90ff07d65e308299e28a5
Niko <nikominicucci@gmail.com>
2020-06-27 04:45:13 -0300

    finish last ejercise

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -51,7 +51,8 @@ function dePalabrasAFrase(palabras) {
   // con espacios entre cada palabra
   // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'
   // Tu código:
-
+  var texto = palabras.join (" ") ;
+    return texto ;
 }
 
 function arrayContiene(array, elemento) {
@@ -67,18 +68,29 @@ function agregarNumeros(numeros) {
   // "array" debe ser una matriz de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0
+  for(i=0 ; i < numeros.length ; i++){
+    suma += numeros [i] ;}
+  return suma;
 }
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var suma = 0
+  for(i=0 ; i < resultadosTest.length ; i++){
+    suma += resultadosTest [i] ;}
+    var promedio = (suma / (resultadosTest.length));
+    return promedio;
 }
 
 function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var maximo = Math.max.apply (null,numeros);
+  return maximo;
 }
 
 function multiplicarArgumentos() {
